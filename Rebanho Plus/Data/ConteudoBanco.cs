4df92aa39726ea36e3bd868eb38a20b572a60cf5
Heitor <Heitor@DESKTOP-4VP11DC>
2026-06-12 17:19:45 -0300

    Adicionado Banco provisório em memória.

---
+++ Rebanho Plus/Data/ConteudoBanco.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rebanho_Plus.Data
+{
+    public class ConteudoBanco: DbContext
+    {
+        
+        
+
+
+
+
+    }
+}
--- Rebanho Plus/Program.cs
+++ Rebanho Plus/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Rebanho_Plus.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ConteudoBanco>(Options => Options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
